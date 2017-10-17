bdc51c0d25af541dd7f6879a8cd4a92f270f894b
Matthew Green <mgreen1313@gmail.com>
2017-10-17 18:22:11 -0400

    feat: Collect price for games and gather info from all pages instead of only the first

--- game_scaper.py
+++ game_scaper.py
@@ -20,34 +20,51 @@ def clean_and_int(data):
             data[i] = int(data[i])
     return data
 
-page = requests.get('https://thinkgaming.com/app-sales-data/?page=1/.html')
-tree = html.fromstring(page.content)
+games = []
+publishers = []
+revenue = []
+installs = []
+free_rank = []
+gross_rank = []
+price = []
 
-# xpath to the game names
-games = tree.xpath('//a[@class="App-Teaser__link"]/text()')
+for i in range(1, 5):
+    page = requests.get('https://thinkgaming.com/app-sales-data/?page={}'.format(i))
+    tree = html.fromstring(page.content)
+    
+    # xpath to the game names
+    games.extend(tree.xpath('//a[@class="App-Teaser__link"]/text()'))
+    
+    # xpath to the game publishers
+    publishers.extend(tree.xpath('//td[@class="info table-data table-data-publisher"]/a/text()'))
+    
+    # xpath to the game's revenue
+    revenue.extend(tree.xpath('//td[@class="table-data table-data-revenue"]/text()'))
+    
+    # xpath for the amount of installs per game    
+    installs.extend(tree.xpath('//td[@class="table-data table-data-installs_new"]/text()'))
+    
+    # xoath for the free_rank(amount of installs) of the game  
+    free_rank.extend(tree.xpath('//td[@class="info table-data table-data-free_rank"]/text() | \
+                           //td[@class="info table-data table-data-free_rank table-data-free_rank_empty"]/text() | \
+                           //td[@class="info table-data table-data-paid_rank"]/text()'))
+    
+    # xpath for the gross_rank of the game
+    gross_rank.extend(tree.xpath('//td[@class="info top-grossing-rank"]/text()'))
+    
+    # xpath for the price of the game
+    price.extend(tree.xpath('//td[@class="info table-data table-data-price"]/text()'))    
 
-# xpath to the game publishers
-publishers = tree.xpath('//td[@class="info table-data table-data-publisher"]/a/text()')
-
-# xpath to the game's revenue
-revenue = tree.xpath('//td[@class="table-data table-data-revenue"]/text()')
+# clean the data   
 revenue = clean_and_int(revenue)
-
-# xpath for the amount of installs per game    
-installs = tree.xpath('//td[@class="table-data table-data-installs_new"]/text()')
 installs = clean_and_int(installs)
-
-# xoath for the free_rank(amount of installs) of the game  
-free_rank = tree.xpath('//td[@class="info table-data table-data-free_rank"]/text() | \
-                       //td[@class="info table-data table-data-free_rank table-data-free_rank_empty"]/text()')
 free_rank = clean_and_int(free_rank)
-
-# xpath for the gross_rank of the game
-gross_rank = tree.xpath('//td[@class="info top-grossing-rank"]/text()')
 gross_rank = clean_and_int(gross_rank)
 
-package = list(zip(games, publishers, revenue, installs, gross_rank, free_rank))
+# combine all the lists together
+data_package = list(zip(games, publishers, revenue, installs, gross_rank, free_rank, price))
 
-print(package)
+print(len(data_package))
+
 # %%
 
