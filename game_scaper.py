d39bbd2f63a911a6c9c5ec8a62ef9925b8e2cf1c
Matthew Green <mgreen1313@gmail.com>
2017-10-18 15:04:17 -0400

    feat: Add date column and 1-200 indexing

--- game_scaper.py
+++ game_scaper.py
@@ -6,8 +6,11 @@ Created on Mon Oct 16 14:36:36 2017
 @author: matthew_green
 """
 
-from lxml import html
 import requests
+from lxml import html
+import pandas as pd
+import os
+import datetime
 
 # helper function that cleans common characters from numbers
 # and changes them to int class
@@ -22,11 +25,11 @@ def clean_and_int(data):
 
 games = []
 publishers = []
-revenue = []
+revenues = []
 installs = []
-free_rank = []
-gross_rank = []
-price = []
+free_ranks = []
+gross_ranks = []
+prices = []
 
 for i in range(1, 5):
     page = requests.get('https://thinkgaming.com/app-sales-data/?page={}'.format(i))
@@ -39,32 +42,47 @@ for i in range(1, 5):
     publishers.extend(tree.xpath('//td[@class="info table-data table-data-publisher"]/a/text()'))
     
     # xpath to the game's revenue
-    revenue.extend(tree.xpath('//td[@class="table-data table-data-revenue"]/text()'))
+    revenues.extend(tree.xpath('//td[@class="table-data table-data-revenue"]/text()'))
     
     # xpath for the amount of installs per game    
     installs.extend(tree.xpath('//td[@class="table-data table-data-installs_new"]/text()'))
     
     # xoath for the free_rank(amount of installs) of the game  
-    free_rank.extend(tree.xpath('//td[@class="info table-data table-data-free_rank"]/text() | \
+    free_ranks.extend(tree.xpath('//td[@class="info table-data table-data-free_rank"]/text() | \
                            //td[@class="info table-data table-data-free_rank table-data-free_rank_empty"]/text() | \
                            //td[@class="info table-data table-data-paid_rank"]/text()'))
     
     # xpath for the gross_rank of the game
-    gross_rank.extend(tree.xpath('//td[@class="info top-grossing-rank"]/text()'))
+    gross_ranks.extend(tree.xpath('//td[@class="info top-grossing-rank"]/text()'))
     
     # xpath for the price of the game
-    price.extend(tree.xpath('//td[@class="info table-data table-data-price"]/text()'))    
+    prices.extend(tree.xpath('//td[@class="info table-data table-data-price"]/text()'))    
 
 # clean the data   
-revenue = clean_and_int(revenue)
+revenues = clean_and_int(revenues)
 installs = clean_and_int(installs)
-free_rank = clean_and_int(free_rank)
-gross_rank = clean_and_int(gross_rank)
+free_ranks = clean_and_int(free_ranks)
+gross_ranks = clean_and_int(gross_ranks)
 
-# combine all the lists together
-data_package = list(zip(games, publishers, revenue, installs, gross_rank, free_rank, price))
+# zip the lists into a total dataset
+dataset = list(zip(games, publishers, revenues, installs, gross_ranks, free_ranks, prices))
 
-print(len(data_package))
+#
+frame = pd.DataFrame(dataset, columns=['game', 'publisher', 'revenue', 'installs', 'gross_rank', 'free_rank', 'price'])
+
+frame.index = range(1, 201)
+
+frame.loc[:, 'date'] = datetime.date.today()
+
+# create a new folder if needed
+if not os.path.exists('pickles'):
+    os.makedirs('pickles')
+
+# save each day as a different pickle file
+now = datetime.datetime.now()
+pickle_file = '/Users/matthew_green/Desktop/version_control/gaming_trend_scraper/pickles/{}_{}_{}.pickle'.format(now.month, now.day, now.year)
+
+frame.to_pickle(pickle_file)
 
 # %%
 
